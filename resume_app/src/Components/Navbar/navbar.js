b78781359b1770fbea313812c0f26cee396eb6a5
Eduardo <davidgripho@gmail.com>
2022-07-18 21:58:45 -0600

    PrimerCommitResume

---
+++ resume_app/src/Components/Navbar/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './navbar.css';
+const Navbar = ()=>{
+return(
+    <nav className='navbar'>
+        <ul>
+          <li>home</li>
+          <li>Experience</li>
+          <li>Skills</li>
+          <li>About</li>
+        </ul>
+      </nav>
+)
+}
+
+export default Navbar;
